86d11f8d25d69d1af7990da1b43f504ff0cebd30
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-08-11 22:34:17 +0530

    feat: add explore categorycard

--- src/pages/Explore/ExplorePage.tsx
+++ src/pages/Explore/ExplorePage.tsx
@@ -1,4 +1,5 @@
 import type { Category } from "../../types/nav";
+import ExploreCategorySection from "./components/ExploreCategorySection";
 import ExploreHeader from "./components/ExploreHeader";
 
 interface ExplorePageProps {
