60e0e1737dee4e30a8a81606578c9cd191ba0763
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-08-11 22:53:39 +0530

    refactor: update allproductpage.tsx

--- src/pages/allProductpage.tsx
+++ src/pages/allProductpage.tsx
@@ -320,7 +320,7 @@ export default function AllProductsPage() {
       Render
   ----------------------------- */
   return (
-    <div className="min-h-screen bg-slate-50/60 px-4 sm:px-6 lg:px-8 py-8 sm:py-10 text-slate-900">
+    <div className="min-h-screen bg-slate-50/60 px-4 sm:px-6 lg:px-8 py-8 sm:py-10 text-slate-900 mt-5">
       <div className="max-w-7xl mx-auto space-y-6">
         {/* TOP BAR */}
         <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4  p-4 sm:p-5 ">
